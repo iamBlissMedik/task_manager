1b46e013552f39b797765a7978ba75e4919de5b3
Blizz <ellamsbliss@gmail.com>
2024-11-16 14:29:03 +0100

    get a task

--- controllers/tasks.js
+++ controllers/tasks.js
@@ -1,6 +1,11 @@
 import Task from "../models/Task.js";
-const getAllTasks = (req, res) => {
-  res.send("all tasks");
+const getAllTasks = async (req, res) => {
+  try {
+    const tasks = await Task.find({});
+    res.status(201).json({ tasks });
+  } catch (error) {
+    res.status(500).json({ msg: error });
+  }
 };
 const createTask = async (req, res) => {
   try {
@@ -10,8 +15,17 @@ const createTask = async (req, res) => {
     res.status(500).json({ msg: error });
   }
 };
-const getTask = (req, res) => {
-  res.json({ id: req.params.id });
+const getTask = async (req, res) => {
+  try {
+    const { id: taskID } = req.params;
+    const task = await Task.findOne({ _id: taskID });
+    res.status(201).json({ task });
+    if (!task) {
+      return res.status(404).json({ msg: `no task with id : ${taskID}` });
+    }
+  } catch (error) {
+    res.status(500).json({ msg: error });
+  }
 };
 const updateTask = (req, res) => {
   res.send("updatetask");
